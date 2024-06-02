2fd278a38a7e284eca66b48e444369455e727bf5
Umit Canbolat <umit@umitcanbolat.nl>
2024-06-02 21:43:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,7 @@ def is_duplicate(new_features, threshold=0.5, top_k=10):
     # Get the top_k similar images
     top_matches = search_similar_images(new_features, top_k=top_k)
     
-    for dist, img_path, features_blob in top_matches:
-        existing_features = np.frombuffer(features_blob, dtype=np.float32)
+    for dist in top_matches:
         if dist < threshold:
             return True
     
